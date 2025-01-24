17892a0db2850db298fcf762d0a10080d5fe3cb2
Qian Chu <97355086+qian-chu@users.noreply.github.com>
2025-01-24 16:30:45 +0100

    Create circle_flash_demo.py

---
+++ circle_flash_demo.py
@@ -0,0 +1,51 @@
+"""
+Minimal example of presentation PC control of Neon.
+Only setup required is to have Neon connected to the companion device and
+the correct address and port specified below.
+Start and stop of recording as well as sending events are fully automated.
+"""
+address = "192.168.144.57"
+port = "8080"
+
+from psychopy import core, visual, event
+import sys
+import time
+from pupil_labs.realtime_api.simple import Device
+
+try:
+    device = Device(address=address, port=port)
+except:
+    print("Could not connect to Neon.")
+    sys.exit()
+
+N = 20
+win = visual.Window(screen=-1, fullscr=True, units="height")
+win.mouseVisible = False
+circle = visual.Circle(win, radius=.2, fillColor="red", lineColor="white")
+text = visual.TextStim(win, text="Press space to start", height=.05)
+
+text.draw()
+win.flip()
+event.waitKeys(keyList=["space"]) 
+win.flip()
+device.recording_start()
+core.wait(5)
+estimate = device.estimate_time_offset()
+clock_offset_ns = round(estimate.time_offset_ms.mean * 1_000_000)
+print(f"Clock offset: {clock_offset_ns/1e6} ms")
+
+for i in range(N):
+    circle.draw()
+    win.flip()
+    device.send_event(
+        "Circle onset",
+        event_timestamp_unix_ns=int(time.time_ns() - clock_offset_ns)
+        )
+    core.wait(1)
+    win.flip()
+    core.wait(1)
+
+device.recording_stop_and_save()
+core.wait(1)
+win.close()
+core.quit()
